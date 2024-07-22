3e3cf3daa0d39f609dd47e4c5e0b13202474804f
Luis519RS <l.gomez@cybercom.com.mx>
2024-07-22 12:46:34 -0600

    offset and text scene tool

--- src/Recursos/UI/SpriteText3D.gd
+++ src/Recursos/UI/SpriteText3D.gd
@@ -1,10 +1,21 @@
 @tool
 extends Sprite3D
 
-@export var textoBlock = "Texto Default"
+@export var offsetLocation = Vector3(0,0,0):
+	set(p_offsetLocation):
+		if p_offsetLocation != offsetLocation:
+			offsetLocation = p_offsetLocation
+			_ready()
+			
+@export var textoBlock = "Texto Default":
+	set(p_textoBlock):
+		if p_textoBlock != textoBlock:
+			textoBlock = p_textoBlock
+			_ready()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Label3D.position = offsetLocation
 	$Label3D.text = textoBlock
 	if $Label3D.text != textoBlock:
 		$Label3D.text = textoBlock

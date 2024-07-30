ca28c12714b9a97911a26f40ed9512875cadde39
UlisesDGM <ulises.delgadillog@gmail.com>
2024-07-30 11:43:19 -0600

    progressbar particular

--- src/Scenes/UI/UI_Particular.gd
+++ src/Scenes/UI/UI_Particular.gd
@@ -14,6 +14,13 @@ extends Control
 @onready var lbl_gasto_valor = $"VBoxContainer/main_container/detalles_container/VBoxContainer/sitio_detalles/VBoxContainer/HBoxContainer/Panel/HBoxContainer/Gasto_container/HBoxContainer/Panel/lbl_gasto_valor"
 @onready var lbl_fecha = $VBoxContainer/main_container/detalles_container/VBoxContainer/sitio_detalles/VBoxContainer/fecha_container/lbl_fecha
 
+@onready var progress_bar = $VBoxContainer/main_container/progress_bar_container/ProgressBar
+@onready var lbl_progress_bar_valor_min = $VBoxContainer/main_container/progress_bar_container/progress_bar_marcadores_container/min_container/lbl_progress_bar_valor_min
+@onready var lbl_progress_bar_valor_max = $VBoxContainer/main_container/progress_bar_container/progress_bar_marcadores_container/max_container/lbl_progress_bar_valor_max
+@onready var lbl_nivel_nombre = $VBoxContainer/main_container/progress_bar_container/lbl_nivel_nombre
+@onready var lbl_valor = $VBoxContainer/main_container/progress_bar_container/ProgressBar/lbl_valor
+
+
 
 @export var new_icon: Texture2D
 @export var texture_online: Texture2D
@@ -74,7 +81,6 @@ func _compare_and_print_selected_site(parametro):
 	var last_selected_id = NavigationManager.last_selected
 	for sitio in datos_sitios:
 		if sitio.id_estacion == last_selected_id:
-			#print("Sitio encontrado:")  # Mensaje de depuración
 			#print_site_details(sitio)
 			set_datos_particular(sitio)
 			return  # Sale de la función una vez encontrado el sitio
@@ -105,6 +111,8 @@ func set_datos_particular(sitio: Estacion):
 		# Obtener la unidad correspondiente al tipo de señal
 		var unidad = unidades.get(_signal.tipo_signal, "")
 		
+		if _signal is Señal and _signal.tipo_signal == 1:
+			set_progress_bar(_signal, unidad)
 		if _signal is Señal and _signal.tipo_signal == 2:
 			lbl_presion.text = _signal.nombre
 			lbl_presion_valor.text = str(_signal.valor) + " " + unidad
@@ -114,4 +122,24 @@ func set_datos_particular(sitio: Estacion):
 			break
 			
 	lbl_fecha.text = GlobalUtils.formatear_fecha(sitio.tiempo)
+	
+func set_progress_bar(_signal: Señal, unidad):
+	progress_bar.min_value = _signal.semaforo["normal"]
+	progress_bar.max_value = _signal.semaforo["critico"]
+	lbl_valor.text = str(_signal.valor) + " " + unidad
+	
+	# Asegurarse de que el valor mínimo visible siempre esté presente
+	var display_value = max(_signal.valor, _signal.semaforo["normal"] + 0.3)
 
+	progress_bar.value = display_value
+	lbl_progress_bar_valor_min.text = str(_signal.semaforo["normal"])
+	lbl_progress_bar_valor_max.text = str(_signal.semaforo["critico"])
+	lbl_nivel_nombre.text = _signal.nombre
+
+	# Cambiar el color de la barra de progreso según el valor de la señal
+	if _signal.valor > _signal.semaforo.normal and _signal.valor <= _signal.semaforo.preventivo:
+		progress_bar.modulate = Color(1, 1, 0) # Amarillo
+	elif _signal.valor > _signal.semaforo.preventivo:
+		progress_bar.modulate = Color(1, 0, 0) # Rojo
+	else:
+		progress_bar.modulate = Color(0, 1, 0) # Verde

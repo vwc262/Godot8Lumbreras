0e0f07930592d0a7d312ec1d664930ac1da90a3e
JaxterZepeda <javierzepeda1796@gmail.com>
2024-07-31 17:43:53 -0600

    Sitios y cielo

--- src/Scripts/Managers/Sitio3D_Manager.gd
+++ src/Scripts/Managers/Sitio3D_Manager.gd
@@ -3,12 +3,13 @@ extends Node3D
 signal IrA				
 
 @export var IdEstacion : int = 0
-@onready var modelo = $Mini_B5
+@onready var modelo = $Mini_3D_03
+
 @onready var labelSitio = $LabelNameSitio
 var estacion: Estacion
 
 func _ready():
-	var node = modelo.get_node("Socket_Camara")
+	var node = modelo.get_node("Mini/Socket_Camara")
 	NavigationManager.AddSiteAnchor(IdEstacion,node.global_position, node.rotation * 180/PI)
 	estacion = GlobalData.get_estacion(IdEstacion)
 	labelSitio.text = "%d - %s" % [estacion.id_estacion, estacion.nombre]

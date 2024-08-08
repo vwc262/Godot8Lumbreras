fe7a7ca84c438ebdd4e356711f070baab22434d8
mh <m.h.virtualwave@gmail.com>
2024-08-08 15:31:47 -0600

    letreros de neon

--- src/Scripts/Environment/TreeVisibility.gd
+++ src/Scripts/Environment/TreeVisibility.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @export var MaxAlpha: float = 0.86
 @onready var sprite_3d = $Sprite3D
+@onready var sprite_3d_2: Sprite3D = $Sprite3D2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,5 @@ func _ready():
 
 func _VisibilityTree(value: float, maxZoom: float, minZoom: float):
 	var deltaAlpha = lerp(MaxAlpha, 0.0, remap(value, minZoom, maxZoom, 0, 1))
-	sprite_3d.modulate.a = deltaAlpha
+	#sprite_3d.modulate.a = deltaAlpha
+	#sprite_3d_2.modulate.a = deltaAlpha

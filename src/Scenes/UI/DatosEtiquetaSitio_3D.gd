e0c21cbfd195bea63a739b7bb40c9f62165dbf1e
Hector <vwc.hector@gmail.com>
2024-08-19 12:54:21 -0600

    click etiqueta descomentado

--- src/Scenes/UI/DatosEtiquetaSitio_3D.gd
+++ src/Scenes/UI/DatosEtiquetaSitio_3D.gd
@@ -56,6 +56,5 @@ func refresh_data():
 		canvas_material.set("shader_parameter/texture_albedo", online_texture if estacion.enlace != 0 else offline_texture)
 
 func _on_EtiquetaClick(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-	#if(event is InputEventMouseButton  and event.double_click):
-		#SceneManager.scroll_scene(SceneManager.TIPO_NIVEL.PARTICULAR,estacion.id_estacion)	
-	pass			
+	if(event is InputEventMouseButton  and event.double_click):
+		SceneManager.scroll_scene(SceneManager.TIPO_NIVEL.PARTICULAR,estacion.id_estacion)	

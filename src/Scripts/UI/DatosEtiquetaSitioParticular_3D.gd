c8d11634f608f4061701dc9fee6c497ac2fa4ac7
Hector <vwc.hector@gmail.com>
2024-08-20 09:39:11 -0600

    etiqueta particular con reimports genrados autom

--- src/Scripts/UI/DatosEtiquetaSitioParticular_3D.gd
+++ src/Scripts/UI/DatosEtiquetaSitioParticular_3D.gd
@@ -1,22 +1,13 @@
 extends Node3D
 
 @export var IdEstacion: int = 0;
-@onready var labelFecha = $Sprite3D/FechaSitio
 @onready var labelNivel = $Sprite3D/Nivel
-@onready var labelNombre = $Sprite3D/NombreSitio
 @onready var canvas_container = $Sprite3D
 
-@export var base_color: Color = Color(27, 115, 202, 255);
-@export var color_rango: Color = Color(112, 112, 112, 255);
-
-const online_texture = preload("res://Recursos/UI/3D/Panel_Nivel_a.png");
-const offline_texture = preload("res://Recursos/UI/3D/Panel_Nivel_b.png");
-
 var estacion: Estacion = null;
-var nivel: Señal = null;
+var segnal: Señal = null;
 var semaforo: Semaforo = null;
 
-@onready var progressbar_material:Material = $Sprite3D/Progressbar3D.material_override
 @onready var canvas_material:Material = $Sprite3D.material_override
 var tweenBlur;
 
@@ -33,27 +24,18 @@ func _on_datos_actualizados(_estaciones: Array[Estacion]):
 	if IdEstacion != 0:
 		estacion = GlobalData.get_estacion(IdEstacion)  # Actualiza los datos del sitio con los nuevos datos
 		for _signal: Señal in estacion.signals.values():
-			if _signal.tipo_signal == 1:
-				nivel = _signal;
+			if _signal.tipo_signal == 3:
+				segnal = _signal;
 				if semaforo == null:
-					semaforo = nivel.semaforo
+					semaforo = segnal.semaforo
 				break;
 	refresh_data();
 
 func refresh_data():
-	if estacion != null and nivel != null:
-		labelNombre.text = "%s" % [estacion.nombre]
-		labelFecha.text = "%s" % [GlobalUtils.formatear_fecha(estacion.tiempo)]
-		labelNivel.text = nivel.nombre + ": " + str(nivel.valor) + " m." if nivel.is_dentro_rango() else "N.D."
-		
-		progressbar_material.set_shader_parameter("color", base_color if nivel.dentro_rango else color_rango)
-		var offset_parameter = 1.0 if nivel.dentro_rango else remap(nivel.valor, 0.0, semaforo.critico, 0.01, 1.0)
-		#progressbar_material.set_shader_parameter("offset", offset_parameter)
-		tweenBlur = TweenManager.init_tween(OnTweenFinished)
-		tweenBlur.tween_property(progressbar_material, "shader_parameter/offset", offset_parameter, 0.2)
-		
-		canvas_container.texture = online_texture if estacion.enlace != 0 else offline_texture
-		canvas_material.set("shader_parameter/texture_albedo", online_texture if estacion.enlace != 0 else offline_texture)
+	if estacion != null and segnal != null:
+		labelNivel.text = segnal.nombre + ": " + str(segnal.valor) + " m." if segnal.is_dentro_rango() else "N.D."
+		labelNivel.text = "G1: %s" % [labelNivel.text]
+
 
 func _on_EtiquetaClick(_camera: Node, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if(event is InputEventMouseButton  and event.double_click):

bab17804944eea0a8e2cb0871a449304c70f8546
mh <m.h.virtualwave@gmail.com>
2024-08-23 16:45:47 -0600

    async save and loading

---
+++ src/Scripts/Async/ResourceAsync.gd
@@ -0,0 +1,12 @@
+extends Thread
+class_name AsyncResourceSaver
+
+var _resource : Resource
+var _path_to_save : String
+
+func _init(resource_to_save:Resource,path:String) -> void:
+	_resource = resource_to_save
+	_path_to_save = path	
+
+func _save_async():
+	ResourceSaver.save(_resource,_path_to_save)		
